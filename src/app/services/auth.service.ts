47b2023bf76c082f071ecadd899bb2a5135678b1
Ali <zaabat97@gmail.com>
2023-05-04 15:11:05 +0200

    Sécurisez vos requêtes

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AuthService {
+    private token = 'My FaceToken';
+
+    getToken(): string {
+        return this.token;
+    }
+}
